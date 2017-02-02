b0647b6959b2f3c9e38ea52a98185f30bc971b63
Chyld Medford <chyld.medford@gmail.com>
2017-02-02 11:36:50 +0530

    populating a dev/prod database using flyway

---
+++ src/main/resources/db/data/V100__populate.sql
@@ -0,0 +1,22 @@
+INSERT INTO students (email) VALUES
+  ('bob@aol.com'),
+  ('sam@aol.com'),
+  ('ace@aol.com');
+
+INSERT INTO teachers (name, age, gender) VALUES
+  ('Alice', 35, 'MALE'),
+  ('Johnn', 21, 'FEMALE'),
+  ('Timbr', 68, 'FEMALE');
+
+INSERT INTO klasses (credits, department, fee, name, semester, teacher_id) VALUES
+  (4, 'SCIENCE', 500.0, 'Physics 101', '2017-01-25 00:00:00', 1),
+  (3, 'ENGINEERING', 550.0, 'Electrical Engineering 101', '2018-01-25 00:00:00', 2),
+  (2, 'SCIENCE', 430.0, 'Calculus 101', '2019-01-25 00:00:00', 2);
+
+INSERT INTO klasses_students VALUES
+  (1, 1),
+  (1, 2),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (3, 1);
--- src/main/resources/db/migration/V6__create_klasses_students_join_table.sql
+++ src/main/resources/db/migration/V6__create_klasses_students_join_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `klasses_students` (
-  `student_id` INT            NOT NULL,
-  `klass_id`   INT            NOT NULL,
+ `klass_id`   INT            NOT NULL,
+ `student_id` INT            NOT NULL,
 
-  CONSTRAINT `fk_student_id1`
-  FOREIGN KEY (`student_id`)
-  REFERENCES `students` (`id`),
+ CONSTRAINT `fk_student_id1`
+ FOREIGN KEY (`student_id`)
+ REFERENCES `students` (`id`),
 
-  CONSTRAINT `fk_klass_id1`
-  FOREIGN KEY (`klass_id`)
-  REFERENCES `klasses` (`id`));
+ CONSTRAINT `fk_klass_id1`
+ FOREIGN KEY (`klass_id`)
+ REFERENCES `klasses` (`id`));
